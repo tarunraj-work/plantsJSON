cb162e87912edd6e152c2c74c54b8417c8aca334
tarun <tarunraj.work@gmail.com>
2022-04-13 22:28:32 -0700

    first commit

---
+++ src/main/java/com/plantflashcards/Application.java
@@ -0,0 +1,11 @@
+package com.plantflashcards;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args)
+    {
+        System.out.println("Hello World!");
+    }
+}
